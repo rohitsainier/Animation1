32468950146935ae34e3026c0db0489d116959f0
Rohit Saini <sketchlearn7@gmail.com>
2020-12-31 21:53:57 +0530

    Initial Commit

---
+++ Animation1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animation1
+//
+//  Created by Rohit Saini on 31/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
